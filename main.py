91775b67628d1c681147ed80367a3bdf64072ecf
philtell <2267507789@qq.com>
2020-06-01 16:06:21 +0800

    remove the main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-# 对database文件夹内图片进行特征提取，建立索引文件featureCNN.h5
-python index.py -database database -index featureCNN.h5
-
-# 使用database文件夹内001_accordion_image_0001.jpg作为测试图片，在database内以featureCNN.h5进行近似图片查找，并显示最近似的3张图片
-python query_online.py -query database/001_accordion_image_0001.jpg -index featureCNN.h5 -result database
